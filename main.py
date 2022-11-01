b2c2e1fa135cfd1cb6615fd87a3651d7aae68166
John Gualteros <gualterosjohn40@gmail.com>
2022-10-31 21:03:07 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+sum_odd_digits = 0
+sum_even_digits = 0
+total = 0
+
+card_number = input("Enter a credit card Number: ")
+card_number = card_number.replace("-", "")
+card_number = card_number.replace(" ", "")
+card_number = card_number[::-1]
+
+for x in card_number[::2]:
+    sum_odd_digits += int(x)
+
+for x in card_number[1::2]:
+    x = int(x) * 2
+    if x >= 10:
+        sum_even_digits += (1 + (x % 10))
+    else:
+        sum_even_digits += x
+
+total = sum_odd_digits + sum_even_digits
+
+if total % 10 == 0:
+    print('Is Valid')
+else:
+    print('Is Invalid')
